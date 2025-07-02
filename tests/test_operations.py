33902aa4a25428959df6cdfefc63c4f3a9580dcf
tusharpatwal001 <thinkdifferent782@gmail.com>
2025-07-02 10:42:21 +0530

    unit test case updated

---
+++ tests/test_operations.py
@@ -0,0 +1,14 @@
+from src.math_operations import add, sub
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 0) == -1
+    assert add(1, 9) == 10
+
+
+def test_sub():
+    assert sub(2, 3) == -1
+    assert sub(2-0) == 2
+    assert sub(5, 3) == 2
+    assert sub(3, 3) == 0
